81e123a77abaff190d706f59382f2f17ebc8d8bc
Jeremy Freeman <the.freeman.lab@gmail.com>
2016-12-06 00:32:03 -0500

    fix config files

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 from unmixer import generate, infer, compare
 from numpy.random import normal
 
+# create coefficients and configuration
+# could optionally load from JSON files (see test/resources for examples)
 config = {'noise': [0.1, 0.1]}
 coeff = {'emission': normal(size=(32,)), 'excitation': normal(size=(5,))}
 
